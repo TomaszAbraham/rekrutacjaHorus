d26b7b93aed297c7faa9f4aaa00a2aa493320032
TomaszAbraham <112939236+TomaszAbraham@users.noreply.github.com>
2022-09-08 20:25:24 +0200

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -6,19 +6,15 @@ import java.util.Optional;
 
 public class Wall implements Structure {
 	private List<CompositeBlock> blocks;
+	
 	//zwraca pierwszy napotkany element o danym kolorze "opakowany" w objekt Optional
 	public Optional findBlockByColor(String color) {
 		if (color == null){
 			throw new IllegalArgumentException("Color is null!");
 		}
-		Optional<CompositeBlock> result = Optional.empty();
-		for(CompositeBlock element: this.blocks) {
-			if( element.getColor().equals(color)) {
-				result = Optional.ofNullable(element);
-				return result;
-			}
-		}
-		return result;
+		return blocks.stream()
+				.filter(b->b.getColor().equals(color))
+				.findAny();
 	}
 
 	// zwraca wszystkie elementy z danego materiału
@@ -26,11 +22,9 @@ public class Wall implements Structure {
 		if (material == null){
 			throw new IllegalArgumentException("Material is null!");
 		}
-		List<CompositeBlock> lista = new LinkedList<>();
-		for(CompositeBlock element: this.blocks) {
-			if( element.getMaterial().equals(material)) lista.add(element);
-		}
-		return lista;
+		return blocks.stream()
+				.filter(b->b.getMaterial().equals(material))
+				.collect(Collectors.toList());
 	}
 
 	//zwraca liczbę wszystkich elementów tworzących strukturę
